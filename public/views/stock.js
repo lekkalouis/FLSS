40e49fe67611d8afb50a839fbd953df8a4148a19
lekkalouis <85458696+lekkalouis@users.noreply.github.com>
2026-02-12 14:27:58 +0200

    Update stock.js

--- public/views/stock.js
+++ public/views/stock.js
@@ -7,9 +7,9 @@ export function initStockView() {
   stockInitialized = true;
   "use strict";
 
-  const LOG_STORAGE_KEY = "fl_stock_log_v1";
-  const MRP_STORAGE_KEY = "fl_stock_mrp_v1";
-  const LOCATION_STORAGE_KEY = "fl_stock_location_v1";
+  const LOG_STORAGE_KEY = "80981557295";
+  const MRP_STORAGE_KEY = "67495329839";
+  const LOCATION_STORAGE_KEY = "80981557295";
   const API_BASE = "/api/v1/shopify";
 
   const rootView = document.getElementById("viewStock");
